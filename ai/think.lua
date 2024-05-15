327151e3d457bcff3782f27491618070e872d940
Ventel Software <mkdeosm@gmail.com>
2024-05-15 08:23:22 +0200

    Think 18.0

--- ai/think.lua
+++ ai/think.lua
@@ -1,210 +1,177 @@
+-- Constants and initial settings
+Idle = false
+SLOW_THINK_PERIOD = 1000
 
--- constants
+KNIFE_PRIMARY_ATTACK_DISTANCE = HUMAN_WIDTH * 2
+KNIFE_ALTERNATIVE_ATTACK_DISTANCE = KNIFE_PRIMARY_ATTACK_DISTANCE * 0.66666666666
+OBJECTIVE_LOOKING_AREA_OFFSET = 2
 
-	Idle = false
+-- Movement-related constants and variables
+SLOW_JUMP_PERIOD = 1000
+SlowJumpTime = 0
 
-	SLOW_THINK_PERIOD = 1000
+ScenarioType = {
+    Walking = 1,
+    PlantingBomb = 2,
+    DefusingBomb = 3,
+    SearchingBomb = 5
+}
 
-	KNIFE_PRIMARY_ATTACK_DISTANCE = HUMAN_WIDTH * 2
-	KNIFE_ALTERNATIVE_ATTACK_DISTANCE = KNIFE_PRIMARY_ATTACK_DISTANCE * 0.66666666666
+Scenario = ScenarioType.Walking
+ChainScenario = ScenarioType.Walking
+LastScenarioChangeTime = 0
 
-	OBJECTIVE_LOOKING_AREA_OFFSET = 2;
-	
--- movement
+Area = nil
+PrevArea = nil
+IsAreaChanged = false
 
-	SLOW_JUMP_PERIOD = 1000
-	SlowJumpTime = 0
-	
-	ScenarioType = {
-		Walking = 1, -- randomly walk on navigation map, nothing more
-		
-		-- cstrike, czero scenarios:
-		
-		PlantingBomb = 2, -- run to random bomb place
-		DefusingBomb = 3, -- search planted c4 at bomb places
-		
-		-- DefendingBomb = 4 -- find and stay near planted c4 at bomb place (terrorist)
-							 -- or find and stay near dropped c4 (backpack) (counter-terrorist)
-		SearchingBomb = 5 -- if bomb was dropped - i need to pick up it (terrorist)
-		
-		-- EscapingFromBomb = 6 -- fuck you
-		
-		-- TODO: add hostages rescuing scenarios for cstrike, czero
-		
-		-- TODO: add VIP escaping scenario for cstrike, czero 
-		
-	}
-	
-	Scenario = ScenarioType.Walking
-	ChainScenarion = ScenarioType.Walking
-	LastScenarioChangeTime = 0
+Chain = {}
+ChainIndex = 0
+ChainFinalPoint = {}
 
-	Area = nil
-	PrevArea = nil
-	IsAreaChanged = false
+STUCK_CHECK_PERIOD = 500
+LastStuckMonitorTime = 0
+LastStuckCheckTime = 0
+StuckWarnings = 0
+UnstuckWarnings = 0
+StuckOrigin = {}
+TriedToUnstuck = false
 
-	Chain = {}
-	ChainIndex = 0
-	ChainFinalPoint = {}
+-- Look-related variables
+LookPoint = {}
 
-	STUCK_CHECK_PERIOD = 500
-	LastStuckMonitorTime = 0
+-- Weapon-related variables
+CurrentWeapon = nil
+LastKnownWeapon = nil
+NeedToBuyWeapons = false
 
-	LastStuckCheckTime = 0
-	StuckWarnings = 0
-	UnstuckWarnings = 0
-	StuckOrigin = {}
-	TryedToUnstuck = false
+-- Attack-related variables
+LastAttackTime = 0
 
--- look
+-- Task-related variables
+IsPlantingBomb = false
+IsDefusingBomb = false
+NeedToDestroy = false
+BreakablePosition = {}
 
-	LookPoint = {}
-	
--- weapons
+-- Common variables
+Origin = {}
+IsSlowThink = false
+LastSlowThinkTime = 0
+IsSpawned = false
+IsEndOfRound = false
+IsBombPlanted = false
+IsBombDropped = false
 
-	CurrentWeapon = nil
-	LastKnownWeapon = nil -- only for hint
+-- Behavior settings
+Behavior = {
+    MoveWhenShooting = true,
+    CrouchWhenShooting = false,
+    MoveWhenReloading = true,
+    AimWhenReloading = true,
+    AlternativeKnifeAttack = false,
+    ReloadDelay = 0,
+    DuckWhenPlantingBomb = false,
+    DuckWhenDefusingBomb = false
+}
 
-	NeedToBuyWeapons = false
+-- Enemy and friend tracking variables
+NearestEnemy = nil
+NearestLeaderEnemy = nil
+EnemiesNearCount = 0
+HasEnemiesNear = false
 
--- attack
+NearestFriend = nil
+NearestLeaderFriend = nil
+FriendsNearCount = 0
+HasFriendsNear = false
 
-	LastAttackTime = 0
-	
--- tasks
+NearestPlayer = nil
+NearestLeaderPlayer = nil
+PlayersNearCount = 0
+HasPlayersNear = false
 
-	IsPlantingBomb = false
-	IsDefusingBomb = false
-
-	NeedToDestroy = false
-	BreakablePosition = {}
-	
--- common
-
-	Origin = {}
-
-	IsSlowThink = false
-	LastSlowThinkTime = 0
-
-	IsSpawned = false
-
-	IsEndOfRound = false
-
-	IsBombPlanted = false
-	IsBombDropped = false
-	
-	Behavior = {
-		MoveWhenShooting = true,
-		CrouchWhenShooting = false,
-		MoveWhenReloading = true,
-		AimWhenReloading = true,
-		AlternativeKnifeAttack = false,
-		ReloadDelay = 0,
-		DuckWhenPlantingBomb = false,
-		DuckWhenDefusingBomb = false
-		
-	}
-
-	NearestEnemy = nil
-	NearestLeaderEnemy = nil
-	EnemiesNearCount = 0
-	HasEnemiesNear = false
-
-	NearestFriend = nil
-	NearestLeaderFriend = nil
-	FriendsNearCount = 0
-	HasFriendsNear = false
-	
-	NearestPlayer = nil
-	NearestLeaderPlayer = nil
-	PlayersNearCount = 0
-	HasPlayersNear = false
-
-dofile "ai/utils.lua" 
-
-dofile "ai/movement.lua" 
-dofile "ai/look.lua" 
+-- Include other necessary modules
+dofile "ai/utils.lua"
+dofile "ai/movement.lua"
+dofile "ai/look.lua"
 dofile "ai/weapons.lua"
 dofile "ai/attack.lua"
 dofile "ai/tasks.lua"
 
+-- Main think function called every frame
 function Think()
-	if IsAlive() then
-		if not IsSpawned then
-			Spawn()
-		end
-		
-		Movement()
-		Look()
-		Weapons()
-		Attack()
-		Tasks()
-	else
-		if IsSpawned then
-			Die()
-		end
-		
-		TryToRespawn()
-	end
-end 
+    if IsAlive() then
+        if not IsSpawned then
+            Spawn()
+        end
+        
+        Movement()
+        Look()
+        Weapons()
+        Attack()
+        Tasks()
+    else
+        if IsSpawned then
+            Die()
+        end
+        
+        TryToRespawn()
+    end
+end
 
+-- PreThink function, called before the main Think function
 function PreThink()
-	Origin = Vec3.New(GetOrigin())
-	
-	IsSlowThink = DeltaTicks(LastSlowThinkTime) >= SLOW_THINK_PERIOD
+    Origin = Vec3.New(GetOrigin())
+    IsSlowThink = DeltaTicks(LastSlowThinkTime) >= SLOW_THINK_PERIOD
 
-	if IsSlowThink then
-		FindEnemiesAndFriends()
-		LastSlowThinkTime = Ticks()
-	end
-	
-	if not IsAlive() then
-		return
-	end
-	
-	FindCurrentWeapon()
-	
-	if not HasEnemiesNear then
-		FindEnemiesAndFriends()
-	end
+    if IsSlowThink then
+        FindEnemiesAndFriends()
+        LastSlowThinkTime = Ticks()
+    end
+    
+    if not IsAlive() then
+        return
+    end
+    
+    FindCurrentWeapon()
+    
+    if not HasEnemiesNear then
+        FindEnemiesAndFriends()
+    end
 end
 
+-- PostThink function, called after the main Think function
 function PostThink()
-	
-	-- decrease recoil
-	
-	V = Vec3.New(GetViewAngles()) - (Vec3.New(GetPunchAngle()) * 0.5) -- aim fix
-	SetViewAngles(Vec3Unpack(V))
+    -- Reduce recoil by adjusting the view angles
+    local V = Vec3.New(GetViewAngles()) - (Vec3.New(GetPunchAngle()) * 0.5)
+    SetViewAngles(Vec3Unpack(V))
 end
 
+-- Handle bot spawning
 function Spawn()
-	IsSpawned = true
-	
-	NeedToBuyWeapons = true
-	
-	Behavior.Randomize()
-	
-	ResetObjectiveMovement()
-	ResetStuckMonitor()
-	
-	print "spawned"
+    IsSpawned = true
+    NeedToBuyWeapons = true
+    Behavior.Randomize()
+    ResetObjectiveMovement()
+    ResetStuckMonitor()
+    print("spawned")
 end
 
+-- Handle bot death
 function Die()
-	IsSpawned = false
-	
-	print "died"
+    IsSpawned = false
+    print("died")
 end
 
+-- Attempt to respawn the bot
 function TryToRespawn()
-	if not IsSlowThink then
-		return
-	end
-	
-	if (GetGameDir() == "valve")
-	or (GetGameDir() == "dmc")
-	or (GetGameDir() == "tfc")
-	or (GetGameDir() == "gearbox") then
-		PrimaryAttack()
-	end
+    if not IsSlowThink then
+        return
+    end
+    
+    local gameDir = GetGameDir()
+    if gameDir == "valve" or gameDir == "dmc" or gameDir == "tfc" or gameDir == "gearbox" then
+        PrimaryAttack()
+    end
 end
